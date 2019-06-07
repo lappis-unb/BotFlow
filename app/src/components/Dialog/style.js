b2b916ab75da50f9a3ecda19bbe87ff82d82d6e4
KahCosta <kamillacosta.unb@gmail.com>
2019-06-07 13:09:10 -0300

    change component box

--- app/src/components/Dialog/style.js
+++ app/src/components/Dialog/style.js
@@ -5,10 +5,11 @@ export const DialogBox = styled.div`
 width: 300px;
 margin: 50px auto;
 background: #F2F2F2;
-padding: 20px;
+padding: 10px;
 text-align: center;
-font-weight: 900;
+font-weight: 800;
 color: #000;
+border-radius: 10px;
 font-family: arial;
 position:relative;
 height:auto;
@@ -17,17 +18,18 @@ box-shadow: rgba(0, 0, 0, 0.3) 2px 2px 2px
 :before {
   content: "";
   position: absolute;
-  box-shadow: rgba(0.0, 0.0, 0.0, 0.0) 2px 2px 2px ;
+  box-shadow: rgba(0.0, 0.0, 0.0, 0.0) 2px 2px 1px ;
   -moz-transform: rotate(280deg);
   -webkit-transform: rotate(280deg);
-  left: -20px;
+  left: -30px;
   top: 20px;
-  border-width: 15px;
+  border-width: 20px;
   border-style: solid;
   border-color: transparent #F2F2F2 #F2F2F2 transparent;
 }
 textarea {
     width: inherit;
+    text-align: center;
     height: 100px;
     background: inherit;
     margin: 0;
@@ -39,5 +41,5 @@ textarea {
 `
 export const Delete = styled(DeleteIcon)`
   color: #4F4F4F;
-  padding-left: 12em;
+  padding-left: 11em;
 `
