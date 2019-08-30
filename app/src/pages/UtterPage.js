97a79a18251f560c10aaf7529c99b82eb8e11e14
Eduardo Nunes <eduardonunes2525@gmail.com>
2019-08-30 00:12:12 -0300

    Creating item page

--- app/src/pages/UtterPage.js
+++ app/src/pages/UtterPage.js
@@ -1,32 +1,8 @@
 import { connect } from "react-redux";
 import React, { Component } from "react";
-import Grid from '@material-ui/core/Grid';
-import { Divider } from "@material-ui/core";
-import Button from '@material-ui/core/Button';
-import ItemsList from "../components/ItemsList";
-import UtterForm from "../components/UtterForm";
-import Snackbar from '@material-ui/core/Snackbar';
-import ToolbarName from '../components/ToolbarName'
-import MessageIcon from '@material-ui/icons/Message';
 import * as utterAction from "../actions/uttersAction";
-import { Add, CreateNewUtter } from '../styles/button';
-import SnackbarContent from "../components/CustomSnackbar"
 
-
-
-const style = {
-  grid_item_list: {
-    background: "#dae8ea"
-  },
-  create_button: {
-    margin: "16px 24px"
-  },
-  item_form: {
-    height: "calc(100vh - 164px)",
-    overflowY: "auto",
-    overflowX: "hidden"
-  }
-}
+import ItemPage from "../pages/ItemPage"
 
 class UtterPage extends Component {
   constructor(props) {
@@ -37,87 +13,20 @@ class UtterPage extends Component {
     this.props.getUtters();
   }
 
-  handleClose() {
-    this.props.notifyAction("");
-  }
-
-  getCreateButton(text) {
-    return (
-      <Button
-        color="secondary"
-        variant="contained"
-        style={style.create_button}
-        onClick={() => this.props.createNewUtter()}>
-        <CreateNewUtter>
-          <Add />
-          <label>{text}</label>
-        </CreateNewUtter>
-      </Button>
-    )
-  }
-
-  getSnackbar(notification_text) {
-    return (
-      <Snackbar
-        anchorOrigin={{
-          vertical: 'bottom',
-          horizontal: 'right',
-        }}
-        autoHideDuration={6000}
-        open={notification_text !== ''}
-        onClose={() => this.handleClose()}>
-        <SnackbarContent
-          variant="success"
-          message={notification_text}
-          onClose={() => this.handleClose()}
-        />
-      </Snackbar>
-    )
-  }
-
   render() {
-    const ITEMS = this.props.utters;
-    const NAME_LABEL = "Nome da resposta";
-    const OLD_ITEM = this.props.old_utter;
-    const HELPER_TEXT = this.props.helper_text;
-    const CREATE_BUTTON_TEXT = "Criar Resposta";
-    const CURRENT_ITEM = this.props.current_utter;
-    const ITEM_LIST_TEXT = "Respostas cadastradas";
-
-    const ITEM_NAME = (this.props.current_item !== undefined) ? this.props.current_item.nameUtter : "";
-
-
-
     return (
-      <Grid container>
-        <Grid item xs={3} style={style.grid_item_list}>
-          {this.getCreateButton(CREATE_BUTTON_TEXT)}
-
-          <ItemsList
-            icon={<MessageIcon />}
-            items={ITEMS}
-            text={ITEM_LIST_TEXT}
-            selected_item_position={this.props.selected_item_position} />
-        </Grid>
-
-        <Grid item xs={9}>
-          <ToolbarName
-            current_item={CURRENT_ITEM}
-            items={ITEMS}
-            old_item={OLD_ITEM}
-            name_label={NAME_LABEL}
-            helper_text={HELPER_TEXT}
-          />
-
-          <Divider />
-
-          <div style={style.item_form}>
-            <UtterForm />
-          </div>
-
-          {this.getSnackbar(this.props.notification_text)}
-        </Grid>
-      </Grid >
+      <ItemPage
+        mode="utter"
+        items={this.props.utters}
+        name_label="Nome da resposta"
+        button_text="Criar nova resposta"
+        old_item={this.props.old_utter}
+        helper_text={this.props.helper_text}
+        item_list_text="Respostas cadastradas"
+        current_item={this.props.current_utter}
+        notification_text={this.props.notification_text}
+        selected_item_position={this.props.selected_item_position}
+      />
     )
   }
 }
@@ -125,9 +34,7 @@ class UtterPage extends Component {
 const mapStateToProps = state => { return { ...state.utterReducer } };
 
 const mapDispatchToProps = dispatch => ({
-  getUtters: () => dispatch(utterAction.getUtters()),
-  createNewUtter: () => dispatch(utterAction.createNewUtter()),
-  notifyAction: (text) => dispatch(utterAction.notifyAction(text)),
+  getUtters: () => dispatch(utterAction.getUtters())
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(UtterPage);
