d7256f9ea65b218147d1fd1a8d0a37ddac112d13
Eduardo_Nunes <eduardonunes2525@gmail.com>
2019-09-16 17:04:13 -0300

    Set api url like docker-compose variable
    Signed-off-by: Eduardo_Nunes <eduardonunes2525@gmail.com>
    Co-authored-by: Arthur Diniz <arthurbdiniz@gmail.com>

--- app/src/utils/url_routes.js
+++ app/src/utils/url_routes.js
@@ -1,6 +1,5 @@
-//export const BASE = "https://botflow.api.lappis.rocks/api/v1/projects/1/";
+export const BASE = process.env.REACT_APP_URL_API + 'api/v1/projects/1/';
 
-export const BASE = process.env.REACT_APP_URL_API;
 export const UTTER_URL = BASE + "utters/";
 export const STORY_URL = BASE + "stories/";
 export const INTENT_URL = BASE + "intents/";
