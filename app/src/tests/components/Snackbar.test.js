e3b2c877265ed912dcdb3f7134a9c5923e901b3e
Amanda Muniz <amanda.muniiz@outlook.com>
2021-04-08 12:59:25 -0300

    [#155] - Adding Snackbar test
    Co-authored-by: Amanda Muniz <amanda.muniiz@outlook.com>
    Co-authored-by: Letícia Araujo <leticiards.04@gmail.com>

--- app/src/tests/components/Snackbar.test.js
+++ app/src/tests/components/Snackbar.test.js
@@ -1,20 +1,13 @@
-import React from 'react';
-import Snackbar from '../../components/Snackbar';
+import MessageSnackbar from '../../components/Snackbar';
 import Adapter from 'enzyme-adapter-react-16';
-import renderer from 'react-test-renderer';
 import Enzyme from 'enzyme';
-import { Provider } from 'react-redux'
-import { createStore } from "redux";
 
 Enzyme.configure({ adapter: new Adapter() });
 
-it('renders correctly', () => {
-    const tree = renderer
-        .create( <Snackbar
-            variant='test'
-            message='test'
-            onClose={() => jest.fn()}
-        />) 
-        .toJSON();
-    expect(tree).toMatchSnapshot();
-});
+describe('Testing SnackBar', () => {
+
+    it('should render as expected', function() {
+      const tree = MessageSnackbar({notification_text: 'test'}, {handleClose: () => jest.fn()}, {variant: "success" });
+      expect(tree).toMatchSnapshot();
+    });
+  });
